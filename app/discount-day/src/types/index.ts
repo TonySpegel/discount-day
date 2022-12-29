2ef9f7fa4566578e9977eabac00e524674e8095c
Tony Spegel <TonySpegel@googlemail.com>
2022-12-29 23:34:57 +0100

    a type for weekdays

---
+++ app/discount-day/src/types/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Copyright © 2022/2023 Tony Spegel
+ */
+
+export { type weekDay } from './weekday.type';
---
+++ app/discount-day/src/types/weekday.type.ts
@@ -0,0 +1,12 @@
+/**
+ * Copyright © 2022/2023 Tony Spegel
+ */
+
+export type weekDay =
+  | 'monday'
+  | 'tuesday'
+  | 'wednesday'
+  | 'thursday'
+  | 'friday'
+  | 'saturday'
+  | 'sunday';
