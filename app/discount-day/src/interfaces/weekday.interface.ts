f22eae8c2429dd250bed09d8ab0595a37dbae100
Tony Spegel <TonySpegel@googlemail.com>
2022-12-29 22:54:09 +0100

    some initial interfaces for weekdays and dishes

---
+++ app/discount-day/src/interfaces/weekday.interface.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright © 2022/2023 Tony Spegel
+ */
+
+import { weekDay } from '../types/';
+
+export interface WeekDayInterface {
+  name: weekDay;
+  today: boolean;
+  weekIndex: number;
+}
