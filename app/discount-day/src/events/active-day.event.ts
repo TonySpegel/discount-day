e582bb15dacdea72dcb2872e5e85cb977c50866a
Tony Spegel <TonySpegel@googlemail.com>
2023-01-04 23:58:14 +0100

    use shared type

--- app/discount-day/src/events/active-day.event.ts
+++ app/discount-day/src/events/active-day.event.ts
@@ -2,12 +2,12 @@
  * Copyright © 2022/2023 Tony Spegel
  */
 
-import { weekDay } from '../types/';
+import { WeekDayType } from '../../../../shared/types/weekday.type.js';
 
 export class ActiveDayEvent extends Event {
   static readonly eventName = 'active-day-event' as const;
 
-  constructor(public activeDay: weekDay) {
+  constructor(public activeDay: WeekDayType) {
     super(ActiveDayEvent.eventName, {
       bubbles: true,
       cancelable: false,
