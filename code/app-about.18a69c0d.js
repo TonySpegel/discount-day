7bcb82bffad11b1e3dce6f092f1f1c07a4f9da44
TonySpegel <TonySpegel@googlemail.com>
2023-01-10 14:59:23 +0000

    Deploying to gh-pages from @ TonySpegel/discount-day@cd6be66a4cf44d1b60899fc1aff60b5a3eb1ec0e 🚀

--- code/app-about.18a69c0d.js
+++ code/app-about.18a69c0d.js
@@ -1,4 +1,4 @@
-import{i as c,s as h,y as u,a as i,e as d}from"./index.ef9f286f.js";const l=c`
+import{i as c,s as h,y as u,a as i,e as d}from"./index.5473ad6e.js";const l=c`
   @media(min-width: 1000px) {
     sl-card {
       max-width: 70vw;
@@ -24,4 +24,4 @@ import{i as c,s as h,y as u,a as i,e as d}from"./index.ef9f286f.js";const l=c`
         </sl-card>
   </main>
     `}};p.styles=[i,l];p=b([d("app-about")],p);export{p as AppAbout};
-//# sourceMappingURL=app-about.50ac16c4.js.map
+//# sourceMappingURL=app-about.18a69c0d.js.map
