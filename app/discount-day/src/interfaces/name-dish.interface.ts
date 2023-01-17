240b59f13a17e19bcd36195d73158842b49d1535
Tony Spegel <TonySpegel@googlemail.com>
2023-01-17 22:43:40 +0100

    extend interface

--- app/discount-day/src/interfaces/name-dish.interface.ts
+++ app/discount-day/src/interfaces/name-dish.interface.ts
@@ -2,7 +2,23 @@
  * Copyright © 2022/2023 Tony Spegel
  */
 
-export interface NameDishInterface {
+type DiscountCategoryType =
+  | 'döner'
+  | 'gemüse döner'
+  | 'pizza'
+  | 'nudeln'
+  | 'falafel'
+  | 'schnitzel'
+  | 'sandwich'
+  | 'salat';
+
+export interface DiscountCardInterface {
+  business: string;
+  coords: {
+    x: number;
+    y: number;
+  };
+  category: DiscountCategoryType;
   name: string;
-  dish_name: string;
+  price: number;
 }
