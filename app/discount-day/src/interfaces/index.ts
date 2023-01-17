b7ea650fd620766f0f55026f7a2aa5fb9e911643
Tony Spegel <TonySpegel@googlemail.com>
2023-01-17 23:57:27 +0100

    rename interface

--- app/discount-day/src/interfaces/index.ts
+++ app/discount-day/src/interfaces/index.ts
@@ -2,5 +2,5 @@
  * Copyright © 2022/2023 Tony Spegel
  */
 
+export { type DiscountCardInterface } from './discount-card.interface';
 export { type WeekDayInterface } from './weekday.interface';
-export { type NameDishInterface } from './name-dish.interface';
