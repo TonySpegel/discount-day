6064869a8b26cf5a90662c1244638c174fcd513c
Tony Spegel <TonySpegel@googlemail.com>
2023-01-22 01:45:04 +0100

    use dotenv

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,26 +1,37 @@
+/**
+ * Copyright © 2022/2023 Tony Spegel
+ */
+
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import express, { Express } from 'express';
 import helmet from 'helmet';
+import path from 'path';
+import type { ParamsDictionary } from 'express-serve-static-core';
+import { fileURLToPath } from 'url';
 
-import { BusinessDish } from './interfaces/business.interface.js';
-import { getDish } from './controller/dayQuery.js';
+import { DiscountCardInterface } from '../../shared/interfaces/discount-card.interface';
+import { getDiscountsPerDay } from './controller/getDiscountsPerDay.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const env = dotenv.config({ path: __dirname + '/.env' });
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
-
-const allowedOrigins = ['http://localhost:3001', 'http://rhea.lan:3001'];
-const options: cors.CorsOptions = {
-  origin: allowedOrigins,
-};
+const port = env.parsed?.PORT || 1234;
+const allowedOrigins = env.parsed?.ALLOWED_ORIGINS.split(',');
 
 app.use(helmet());
-app.use(cors(options));
+app.use(cors({ origin: allowedOrigins }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get<BusinessDish>('/day', getDish); // current day
-app.get<BusinessDish>('/day/:weekDay', getDish); // /?day=friday
+app.get<ParamsDictionary, DiscountCardInterface[]>('/day', getDiscountsPerDay); // current day
+app.get<ParamsDictionary, DiscountCardInterface[]>(
+  '/day/:weekDay',
+  getDiscountsPerDay
+); // /?day=friday
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
