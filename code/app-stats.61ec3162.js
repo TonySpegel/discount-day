80346e11bfb9e678cc25395453039c06651a45b0
TonySpegel <TonySpegel@googlemail.com>
2023-02-11 20:07:47 +0000

    Deploying to gh-pages from @ TonySpegel/discount-day@1750a070d37b63a38dfec3c1544a0ff6862447f9 🚀

--- code/app-stats.61ec3162.js
+++
@@ -1,7 +0,0 @@
-import{s as h,h as d,g as l,y as p,t as u,e as _}from"./index.11615475.js";var f=Object.defineProperty,g=Object.getOwnPropertyDescriptor,c=(e,r,n,t)=>{for(var s=t>1?void 0:t?g(r,n):r,o=e.length-1,a;o>=0;o--)(a=e[o])&&(s=(t?a(r,n,s):a(s))||s);return t&&s&&f(r,n,s),s};let i=class extends h{constructor(){super();this.response="",this._apiTask=new d(this,()=>l("https://api.discountday.duckdns.org/test"),()=>[this.response]),console.log("https://api.discountday.duckdns.org/test")}render(){return p`
-      <h2>stats</h2>
-      <main>
-        ${this._apiTask.render({pending:()=>p`<sl-spinner></sl-spinner>`,error:()=>p`error`,complete:e=>p`${e}`})}
-      </main>
-    `}};c([u()],i.prototype,"response",2);i=c([_("app-stats")],i);export{i as AppStats};
-//# sourceMappingURL=app-stats.61ec3162.js.map
